8f71a7630b88411d8cff98e54e3b90539b3035fa
ajmalcvaju <ajmalmayanad@gmail.com>
2025-03-01 20:21:39 +0530

    final-3

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use("/api/parsed-data", parsingRoutes);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
